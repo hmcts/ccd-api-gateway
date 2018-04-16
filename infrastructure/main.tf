c831d484ddffc2cf651d64e8025f89a503804213
Adrian Cho <waihung.cho@hmcts.net>
2018-04-16 14:09:34 +0100

    Code updates:

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,15 +6,11 @@ locals {
   app_full_name = "${var.product}-${var.component}"
   env_ase_url = "${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
 
-  default_document_management_url = "https://api-gateway.${local.env_ase_url}.dm.reform.hmcts.net"
-  default_external_host_name = "gateway-ccd.${local.env_ase_url}.platform.hmcts.net"
-  default_cors_origin = "https://www-ccd.${local.env_ase_url}.platform.hmcts.net"
-  default_ccd_print_service_url = "https://return-case-doc-ccd.${local.env_ase_url}.platform.hmcts.net"
+  default_app_env_ase_url = "https://${local.app_full_name}-${local.env_ase_url}"
 
-  document_management_url = "${var.document_management_url != "" ? var.document_management_url : local.default_document_management_url}"
-  ccd_print_service_url = "${var.ccd_print_service_url != "" ? var.ccd_print_service_url : local.default_ccd_print_service_url}"
-  cors_origin = "${var.cors_origin != "" ? var.cors_origin : local.default_cors_origin}"
-  external_host_name = "${var.external_host_name != "" ? var.external_host_name : local.default_external_host_name}"
+  ccd_print_service_url = "${var.ccd_print_service_url != "" ? var.ccd_print_service_url : local.default_app_env_ase_url}"
+  cors_origin = "${var.cors_origin != "" ? var.cors_origin : local.default_app_env_ase_url}"
+  external_host_name = "${var.external_host_name != "" ? var.external_host_name : local.default_app_env_ase_url}"
 }
 
 data "vault_generic_secret" "address_lookup_token" {
@@ -56,7 +52,7 @@ module "api-gateway-web" {
     PROXY_DATA = "http://ccd-data-store-api-${local.env_ase_url}"
     PROXY_DEFINITION_IMPORT = "http://ccd-definition-store-api-${local.env_ase_url}"
     PROXY_DOCUMENT_MANAGEMENT = "${var.document_management_url}"
-    PROXY_PRINT_SERVICE = "${var.ccd_print_service_url}"
+    PROXY_PRINT_SERVICE = "${local.ccd_print_service_url}"
     WEBSITE_NODE_DEFAULT_VERSION = "8.9.4"
   }
 }
