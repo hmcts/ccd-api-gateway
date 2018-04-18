451cba385a55766377101deabaf67cf14ca40d05
Valentin Laurin <me@vlaurin.eu>
2018-04-18 10:45:50 +0100

    Add missing `local.` prefix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,8 +7,8 @@ locals {
   env_ase_url = "${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
 
   default_app_env_ase_url = "${local.app_full_name}-${local.env_ase_url}"
-  default_ccd_print_service_url = "https://ccd-case-print-service-${env_ase_url}"
-  default_cors_origin = "https://ccd-case-management-web-${env_ase_url}"
+  default_ccd_print_service_url = "https://ccd-case-print-service-${local.env_ase_url}"
+  default_cors_origin = "https://ccd-case-management-web-${local.env_ase_url}"
 
   external_host_name = "${var.external_host_name != "" ? var.external_host_name : local.default_app_env_ase_url}"
 
