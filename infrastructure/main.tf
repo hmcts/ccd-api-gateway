b06faa61d017a8cc9ff741ef9717b6e5c6b42687
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-08-13 10:02:42 +0100

    Vault: remove Hashi vault keys

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,16 +34,19 @@ data "azurerm_key_vault" "ccd_shared_key_vault" {
   resource_group_name = "${local.vaultName}"
 }
 
-data "vault_generic_secret" "address_lookup_token" {
-  path = "secret/${var.vault_section}/ccd/postcode-info/token"
+data "azurerm_key_vault_secret" "address_lookup_token" {
+  name = "postcode-info-address-lookup-token"
+  vault_uri = "${data.azurerm_key_vault.ccd_shared_key_vault.vault_uri}"
 }
 
-data "vault_generic_secret" "oauth2_client_secret" {
-  path = "secret/${var.vault_section}/ccidam/idam-api/oauth2/client-secrets/ccd-gateway"
+data "azurerm_key_vault_secret" "oauth2_client_secret" {
+  name = "ccd-api-gateway-oauth2-client-secret"
+  vault_uri = "${data.azurerm_key_vault.ccd_shared_key_vault.vault_uri}"
 }
 
-data "vault_generic_secret" "idam_service_key" {
-  path = "secret/${var.vault_section}/ccidam/service-auth-provider/api/microservice-keys/ccd-gw"
+data "azurerm_key_vault_secret" "idam_service_key" {
+  name = "ccd-api-gateway-idam-service-key"
+  vault_uri = "${data.azurerm_key_vault.ccd_shared_key_vault.vault_uri}"
 }
 
 module "api-gateway-web" {
@@ -61,14 +64,14 @@ module "api-gateway-web" {
   app_settings = {
     IDAM_OAUTH2_TOKEN_ENDPOINT = "${var.idam_api_url}/oauth2/token"
     IDAM_OAUTH2_CLIENT_ID = "ccd_gateway"
-    IDAM_OAUTH2_CLIENT_SECRET = "${data.vault_generic_secret.oauth2_client_secret.data["value"]}"
+    IDAM_OAUTH2_CLIENT_SECRET = "${data.azurerm_key_vault_secret.oauth2_client_secret.value}"
     IDAM_OAUTH2_LOGOUT_ENDPOINT = "${var.idam_api_url}/session/:token"
-    ADDRESS_LOOKUP_TOKEN = "${data.vault_generic_secret.address_lookup_token.data["value"]}"
+    ADDRESS_LOOKUP_TOKEN = "${data.azurerm_key_vault_secret.address_lookup_token.value}"
     CORS_ORIGIN_METHODS = "GET,POST,OPTIONS"
     CORS_ORIGIN_WHITELIST = "${local.default_cors_origin},${local.cors_origin}"
     IDAM_BASE_URL = "${var.idam_api_url}"
     IDAM_S2S_URL = "${local.s2s_url}"
-    IDAM_SERVICE_KEY = "${data.vault_generic_secret.idam_service_key.data["value"]}"
+    IDAM_SERVICE_KEY = "${data.azurerm_key_vault_secret.idam_service_key.value}"
     IDAM_SERVICE_NAME = "ccd_gw"
     PROXY_AGGREGATED = "http://ccd-data-store-api-${local.env_ase_url}"
     PROXY_CASE_ACTIVITY = "http://ccd-case-activity-api-${local.env_ase_url}"
@@ -78,24 +81,4 @@ module "api-gateway-web" {
     PROXY_PRINT_SERVICE = "${local.ccd_print_service_url}"
     WEBSITE_NODE_DEFAULT_VERSION = "8.9.4"
   }
-}
-
-// Copy into Azure Key Vault
-
-resource "azurerm_key_vault_secret" "address_lookup_token" {
-  name = "postcode-info-address-lookup-token"
-  value = "${data.vault_generic_secret.address_lookup_token.data["value"]}"
-  vault_uri = "${data.azurerm_key_vault.ccd_shared_key_vault.vault_uri}"
-}
-
-resource "azurerm_key_vault_secret" "oauth2_client_secret" {
-  name = "ccd-api-gateway-oauth2-client-secret"
-  value = "${data.vault_generic_secret.oauth2_client_secret.data["value"]}"
-  vault_uri = "${data.azurerm_key_vault.ccd_shared_key_vault.vault_uri}"
-}
-
-resource "azurerm_key_vault_secret" "idam_service_key" {
-  name = "ccd-api-gateway-idam-service-key"
-  value = "${data.vault_generic_secret.idam_service_key.data["value"]}"
-  vault_uri = "${data.azurerm_key_vault.ccd_shared_key_vault.vault_uri}"
 }
