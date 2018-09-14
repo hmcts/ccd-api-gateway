b1edd649c4c0098587ac5e11443e921a5642e489
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-09-14 14:49:09 +0100

    Cleanup: use correct repo names in modules

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -54,7 +54,7 @@ data "azurerm_key_vault_secret" "idam_service_key" {
 }
 
 module "api-gateway-web" {
-  source = "git@github.com:hmcts/moj-module-webapp?ref=master"
+  source = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product = "${var.product}-${var.component}"
   location = "${var.location}"
   env = "${var.env}"
