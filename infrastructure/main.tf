425d5af22a308f07bc480219b2ff7c947bcd58df
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2019-01-17 12:15:09 +0000

    Use shared App Insight for all envs
    This reverts commit eb2f62cfa015b00168b89fd3ed5dc52e4f590ff9.
     - "Enable AppInsight switching between shared/per app"

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -77,7 +77,7 @@ module "api-gateway-web" {
   asp_rg = "${(var.asp_rg == "use_shared") ? local.sharedASPResourceGroup : var.asp_rg}"
   website_local_cache_sizeinmb = 800
   capacity = "${var.capacity}"
-  appinsights_instrumentation_key = "${(var.use_shared_appinsight == "true") ? var.appinsights_instrumentation_key : ""}"
+  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
 
   app_settings = {
     IDAM_OAUTH2_TOKEN_ENDPOINT = "${var.idam_api_url}/oauth2/token"
