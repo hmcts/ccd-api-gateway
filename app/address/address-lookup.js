354cbd5542dd6c1f6da727a3a18e78ffe1d25171
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-03-11 13:19:32 +0000

    Fixing address look up token issue

--- app/address/address-lookup.js
+++ app/address/address-lookup.js
@@ -26,7 +26,7 @@ function getHttpConfig() {
   const httpConfig = {
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Token ' + config.get<string>('secrets.ccd.postcode-info-address-lookup-token')
+      'Authorization': 'Token ' + config.get('secrets.ccd.postcode-info-address-lookup-token')
     }
   };
   if (config.address_lookup.detect_proxy && process.env.https_proxy) {
