98b346993e3612024713420f676965b30713bbf2
Odukoya <764106@cognizant.com>
2019-06-14 12:04:54 +0100

    CCD-gateway-Updated

--- app.js
+++ app.js
@@ -115,6 +115,11 @@ applyProxy(app, {
   ]
 });
 
+applyProxy(app, {
+  source: '/refdata/**'
+  target: config.get('proxy.refdata')
+})
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   let err = new Error('Not Found');
