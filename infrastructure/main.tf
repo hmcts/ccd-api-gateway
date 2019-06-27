5e04125d1b149a4d1dfbd0394df6d6c0c12ef793
Ganeshraja Ramu <ganeshraja.ramu@hmcts.net>
2019-06-27 20:26:40 +0100

    dummy value to add force push vault changes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -55,6 +55,11 @@ data "azurerm_key_vault_secret" "address_lookup_token" {
   key_vault_id = "${data.azurerm_key_vault.ccd_shared_key_vault.id}"
 }
 
+data "azurerm_key_vault_secret" "dummy1" {
+  name = "dumy1"
+  key_vault_id = "${data.azurerm_key_vault.ccd_shared_key_vault.id}"
+}
+
 data "azurerm_key_vault_secret" "oauth2_client_secret" {
   name = "ccd-api-gateway-oauth2-client-secret"
   key_vault_id = "${data.azurerm_key_vault.ccd_shared_key_vault.id}"
