f43862c78d6be3b596271d4aae7b9146417389f0
Tobias <it.consulting@tutanota.com>
2019-09-11 15:41:02 +0100

    RDM-5608: fix CCD Gateway returning wrong 500 status code.
        [RDM-5608] (https://tools.hmcts.net/jira/browse/RDM-5608).

---
+++ app/util/class-attributes-helper.js
@@ -0,0 +1,11 @@
+
+function hasAnAttribute(object,attributeName){
+
+  if('undefined' !== typeof(object[attributeName])){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+module.exports = hasAnAttribute;
