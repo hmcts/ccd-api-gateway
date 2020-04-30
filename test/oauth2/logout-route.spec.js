ab64c79b29de88900b4aa4f229950fb35954f1ce
RBaker <rebecca.baker@hmcts.net>
2020-04-30 10:07:19 +0100

    RDM-8303 added unit tests for logout-route.js

--- test/oauth2/logout-route.spec.js
+++ test/oauth2/logout-route.spec.js
@@ -3,16 +3,24 @@ const expect = chai.expect;
 const fetchMock = require('fetch-mock');
 const proxyquire = require('proxyquire');
 const sinon = require('sinon');
+const assert = sinon.assert;
 const sinonChai = require('sinon-chai');
 const sinonExpressMock = require('sinon-express-mock');
 const ACCESS_TOKEN_COOKIE_NAME = require('../../app/oauth2/oauth2-route').COOKIE_ACCESS_TOKEN;
 chai.use(sinonChai);
+const nock = require('nock');
+const CacheService = require('../../app/cache/cache-service');
 
 describe('logoutRoute', () => {
   const CLIENT_ID = 'ccd_gateway';
   const CLIENT_SECRET = 'abc123def456';
   const ACCESS_TOKEN = 'eycdjc7hf3478g4f37';
   const LOGOUT_END_POINT = 'http://localhost/session/:token';
+  const CACHE_TTL_SECONDS = 1800;
+  const TOKEN = 'TOKEN';
+  const USER_DETAILS = { user: 'Details' };
+  const DETAILS_PATH = '/o/userinfo';
+  const URL = 'http://localhost:5000';
 
   let request;
   let response;
@@ -20,11 +28,25 @@ describe('logoutRoute', () => {
   let config;
   let logoutRoute;
   let fetch;
+  let userInfoCacheSpy;
+  let sandbox;
+  let clock;
+
+  let cachedUserResolver;
+  let userInfoCache;
 
   beforeEach(() => {
     config = {
       get: sinon.stub()
     };
+    sandbox = sinon.sandbox.create();
+    clock = sandbox.useFakeTimers();
+    userInfoCache = new CacheService(CACHE_TTL_SECONDS, 120);
+    userInfoCacheSpy = sandbox.spy(userInfoCache, 'get');
+    cachedUserResolver = proxyquire('../../app/user/cached-user-resolver', {
+      '../cache/cache-config': { userInfoCache }
+    });
+
     config.get.withArgs('idam.oauth2.client_id').returns(CLIENT_ID);
     config.get.withArgs('secrets.ccd.ccd-api-gateway-oauth2-client-secret').returns(CLIENT_SECRET);
     config.get.withArgs('idam.oauth2.logout_endpoint').returns(LOGOUT_END_POINT);
@@ -45,6 +67,14 @@ describe('logoutRoute', () => {
     }).logoutRoute;
   });
 
+  afterEach(() => {
+    sandbox.restore();
+    clock.restore();
+    if (!nock.isDone()) {
+      chai.assert.fail('Not all nock interceptors have completed');
+    }
+  });
+
   it('should call IDAM OAuth 2 logout endpoint with JWT token', done => {
     response.status.callsFake(() => {
       try {
@@ -52,6 +82,17 @@ describe('logoutRoute', () => {
         expect(fetch.lastOptions().headers['Authorization']).to.equal('Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'));
         expect(next).not.to.be.called;
         expect(response.clearCookie).to.be.calledWith(ACCESS_TOKEN_COOKIE_NAME);
+
+        initNock(TOKEN, USER_DETAILS);
+        cachedUserResolver.getUserDetails(TOKEN);
+        clock.tick(CACHE_TTL_SECONDS * 1000 + 1);
+        initNock(TOKEN, USER_DETAILS);
+
+        const result = cachedUserResolver.getUserDetails(TOKEN);
+        expect(JSON.stringify(result)).to.equal('{}');
+        assert.calledWith(userInfoCacheSpy, TOKEN, sinon.match.func);
+        assert.calledTwice(userInfoCacheSpy);
+
         expect(response.status).to.be.calledWith(204);
         done();
       } catch (e) {
@@ -93,4 +134,14 @@ describe('logoutRoute', () => {
       message: 'No auth token to log out'
     });
   });
+
+  const initNock = (token, details) => {
+    nock(URL, {
+      reqheaders: {
+        Authorization: `Bearer ${token}`
+      }
+    }).get(DETAILS_PATH)
+      .times(1)
+      .reply(200, details);
+  };
 });
