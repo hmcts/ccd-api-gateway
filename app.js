d029b0faa6b485984240aa05230e09a2eff48a14
Tim Jacomb <tim.jacomb@hmcts.net>
2020-05-06 08:57:57 +0100

    Revert heading messing with

--- app.js
+++ app.js
@@ -53,11 +53,6 @@ const applyProxy = (app, config) => {
   if (config.source === '/documents') {
     options.onProxyReq = (proxyReq, req) => {
       console.log('proxyReq headers: ', req.headers);
-      proxyReq.setHeader('Accept-Ranges', 'none');
-    };
-    options.onProxyRes = (proxyRes, req, res) => {
-      console.log('proxyRes headers: ',res.headers);
-      proxyRes.headers['Accept-Ranges'] = 'none';
     };
   }
 
