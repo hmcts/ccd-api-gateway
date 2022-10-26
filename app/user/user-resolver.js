4020595415adf4f8b98b488f31a676513d4d46a5
Mike Brown <michael.brown@hmcts.net>
2022-10-26 09:13:34 +0100

    CCD-3837 Http 500 Errors WIP getUserDetails logging

--- app/user/user-resolver.js
+++ app/user/user-resolver.js
@@ -1,14 +1,20 @@
 const config = require('config');
 const fetch = require('../util/fetch');
 const jwtUtil = require('../util/jwt');
+const { Logger } = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('userResolver');
 
 const getUserDetails = (jwt) => {
+  logger.error('getUserDetails inside');
   return fetch(`${config.get('idam.base_url')}/o/userinfo`, {
     headers: {
       'Authorization': jwtUtil.addBearer(jwt)
     }
   })
-  .then(res => res.json());
+  .then(res => res.json())
+  // .finally(res => logger.error('getUserDetails res: ', res, ' res.json: ', res.json()));
+  .finally(res => logger.error('getUserDetails res: ', res));
+  // .finally(res => logger.error('getUserDetails finally'));
 };
 
 exports.getUserDetails = getUserDetails;
