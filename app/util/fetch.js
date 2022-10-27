c9af0d267aab62b433c663591adf257fcf4203f5
Mike Brown <michael.brown@hmcts.net>
2022-10-27 15:04:48 +0100

    fetch dev logging added

--- app/util/fetch.js
+++ app/util/fetch.js
@@ -1,12 +1,18 @@
 const _fetch = require('node-fetch');
+const { Logger } = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('fetch');
 
 const fetch = (...args) => {
+  logger.error('fetch inside');
   return _fetch(...args)
     .then(res => {
 
       if (res.status >= 200 && res.status < 300) {
           return res;
       }
+
+      logger.error('fetch before res: ', res)
+
 
       return Promise.reject(res);
     });
