eddb4b2e64f55b58153c6b2f59a149958c5a2f9e
mikebrownccd <104495891+mikebrownccd@users.noreply.github.com>
2022-11-22 17:05:43 +0000

    Ccd 3837 dns issues (#451)
    * CCD-3837 Http 500 Errors WIP getUserDetails logging
    
    * CCD-3837 debugging logging tweaked
    
    * log message changed
    
    * logging changed
    
    * fetch dev logging added
    
    * more fetch dev logging added
    
    * catch logging added for dev
    
    * more catch and finally logging added for dev
    
    * end added to logging lines
    
    * detch catch removed
    
    * user resolver catch removed
    
    * code added to return 502 if getaddrinfo ENOTFOUND
    
    * isBadGateway logic added to authCheckerUserOnlyFilter
    
    * dev logging added to catch other service calls
    
    * error message changed for dev
    
    * isBadGatewayError() added to applyProxy for data store requests
    
    * ECONNREFUSED added to isBadGatewayError
    
    * refactored mapErrors function created
    
    * mapFetchErrors added to auth-checker-user-only-filter
    
    * methods removed from app.js
    
    * mapFetchErrors called required const
    
    * mapFetchErrors exported
    
    * undefined check added
    
    * error obj now passed instead of empty err
    
    * variable renamed error
    
    * dev logging added
    
    * more dev logging added
    
    * reverted some of the refactoring
    
    * dev logging error now
    
    * extra error properties added to logging
    
    * mapFetchErrors added back to app.js
    
    * attempt to put back authCheckerUserOnlyFilter.mapFetchErrors in app.js
    
    * revert auth-checker-user-only-filter.mapFetchErrors out of app.js
    
    * mapFetchErrors no longer used
    
    * undefined checks added, mapFetchErrors removed
    
    * mapErrors function back for auth-checker, tests added
    
    * mapFetchErrors back in app.js
    
    * lint quotes and semicolon issues fixes
    
    * another attempt to use mapFetchErrors function from auth-checker
    
    * fixed lint errors
    
    * bracket commented
    
    * mapFetchErrors() imported and tests fixed to use filter.authCheckerUserOnlyFilter()
    
    * mapFetchErrors now handles next being undefined
    
    * let next removed
    
    * revert json assignment cleaned up
    
    * refactor in usage on mapFetchErrors
    
    * mapFetchErrors usage reverted, logging added for typeof res
    
    * more logging for res and next
    
    * code cleaned up, tests added for next undefined
    
    * error.message now set when next undefined, tests updated
    
    * dev logging removed
    
    * tests added for "getaddrinfo ENOTFOUND"

--- app/user/auth-checker-user-only-filter.js
+++ app/user/auth-checker-user-only-filter.js
@@ -4,7 +4,6 @@ const { Logger } = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('authCheckerUserOnlyFilter');
 
 const authCheckerUserOnlyFilter = (req, res, next) => {
-
   req.authentication = {};
 
   userRequestAuthorizer
@@ -18,11 +17,7 @@ const authCheckerUserOnlyFilter = (req, res, next) => {
     .catch(error => {
       if (error.name === 'FetchError') {
         logger.error(error);
-        next({
-          status: 500,
-          error: 'Internal Server Error',
-          message: error.message
-        });
+        mapFetchErrors(error, res, next);
       } else {
         logger.warn('Unsuccessful user authentication', error);
         error.status = error.status || 401;
@@ -31,4 +26,52 @@ const authCheckerUserOnlyFilter = (req, res, next) => {
     });
 };
 
-module.exports = authCheckerUserOnlyFilter;
+const isBadGatewayError = (error) => {
+  return error.message !== undefined && (error.message.includes('getaddrinfo ENOTFOUND') || 
+  error.message.includes('socket hang up') ||
+  error.message.includes('getaddrinfo EAI_AGAIN') ||
+  error.message.includes('connect ETIMEOUT') ||
+  error.message.includes('ECONNRESET') ||
+  error.message.includes('ECONNREFUSED'));
+};
+
+const mapFetchErrors = (error, res, next) => {
+  if (next !== undefined){
+    if (isBadGatewayError(error)){
+      next({
+      error: 'Bad Gateway',
+      status: 502,
+      message: error.message
+      });
+    }
+    else {
+      next({
+      error: 'Internal Server Error',
+      status: 500,
+      message: error.message
+      });
+    }
+  } else {
+    if (isBadGatewayError(error)) {
+      res.status(502);
+      res.json({
+        error: 'Bad Gateway',
+        status: 502,
+        message: error.message
+      });
+    }
+    else {
+      res.status(500);
+      res.json({
+        error: 'Error when connecting to remote server',
+        status: 504,
+        message: error.message
+      });
+    }
+  }
+};
+
+module.exports = {
+  authCheckerUserOnlyFilter,
+  mapFetchErrors
+};
--- test/user/auth-checker-user-only-filter.spec.js
+++ test/user/auth-checker-user-only-filter.spec.js
@@ -22,7 +22,14 @@ describe('authCheckerUserOnlyFilter', () => {
     req = {
       headers: {}
     };
-    res = {};
+    res = {
+      status: function(statusInput) {
+        this.status = statusInput;
+      },
+      json: function(jsonInput) {
+        this.json = jsonInput;
+      }
+    };
 
     userRequestAuthorizer = {
       authorise: sinon.stub()
@@ -39,28 +46,28 @@ describe('authCheckerUserOnlyFilter', () => {
     });
 
     it('should call next middleware without error', done => {
-      filter(req, res, error => {
+      filter.authCheckerUserOnlyFilter(req, res, error => {
         expect(error).to.be.undefined;
         done();
       });
     });
 
     it('should set authenticated user in request', done => {
-      filter(req, res, () => {
+      filter.authCheckerUserOnlyFilter(req, res, () => {
         expect(req.authentication.user).to.equal(user);
         done();
       });
     });
 
     it('should add user ID as a request header', done => {
-      filter(req, res, () => {
+      filter.authCheckerUserOnlyFilter(req, res, () => {
         expect(req.headers['user-id']).to.equal(user.uid);
         done();
       });
     });
 
     it('should add comma separated roles as a request header', done => {
-      filter(req, res, () => {
+      filter.authCheckerUserOnlyFilter(req, res, () => {
         expect(req.headers['user-roles']).to.equal(user.roles.join(','));
         done();
       });
@@ -77,8 +84,9 @@ describe('authCheckerUserOnlyFilter', () => {
       };
       userRequestAuthorizer.authorise.returns(Promise.reject(error));
 
-      filter(req, res, error => {
+      filter.authCheckerUserOnlyFilter(req, res, error => {
         expect(error).to.equal(error);
+        // expect(error.status).to.equal(403);
         done();
       });
     });
@@ -91,7 +99,7 @@ describe('authCheckerUserOnlyFilter', () => {
       };
       userRequestAuthorizer.authorise.returns(Promise.reject(error));
 
-      filter(req, res, error => {
+      filter.authCheckerUserOnlyFilter(req, res, error => {
         expect(error.status).to.equal(500);
         expect(error.error).to.equal('Internal Server Error');
         expect(error.message).to.equal('some message');
@@ -99,11 +107,133 @@ describe('authCheckerUserOnlyFilter', () => {
       });
     });
 
+    it('should return 502 status code in case of FetchError and error message containing "getaddrinfo ENOTFOUND" and no status', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah getaddrinfo ENOTFOUND blah'
+      };
+      userRequestAuthorizer.authorise.returns(Promise.reject(error));
+
+      filter.authCheckerUserOnlyFilter(req, res, error => {
+        expect(error.status).to.equal(502);
+        expect(error.error).to.equal('Bad Gateway');
+        expect(error.message).to.equal('blah getaddrinfo ENOTFOUND blah');
+        done();
+      });
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "socket hang up" and no status', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah socket hang up blah'
+      };
+      userRequestAuthorizer.authorise.returns(Promise.reject(error));
+
+      filter.authCheckerUserOnlyFilter(req, res, error => {
+        expect(error.status).to.equal(502);
+        expect(error.error).to.equal('Bad Gateway');
+        expect(error.message).to.equal('blah socket hang up blah');
+        done();
+      });
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "socket hang up" and 403 status', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah socket hang up blah',
+        status: 403
+      };
+      userRequestAuthorizer.authorise.returns(Promise.reject(error));
+
+      filter.authCheckerUserOnlyFilter(req, res, error => {
+        expect(error.status).to.equal(502);
+        expect(error.error).to.equal('Bad Gateway');
+        expect(error.message).to.equal('blah socket hang up blah');
+        done();
+      });
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "getaddrinfo EAI_AGAIN" and no status', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah getaddrinfo EAI_AGAIN blah'
+      };
+      userRequestAuthorizer.authorise.returns(Promise.reject(error));
+
+      filter.authCheckerUserOnlyFilter(req, res, error => {
+        expect(error.status).to.equal(502);
+        expect(error.error).to.equal('Bad Gateway');
+        expect(error.message).to.equal('blah getaddrinfo EAI_AGAIN blah');
+        done();
+      });
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "connect ETIMEOUT" and no status', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah connect ETIMEOUT blah'
+      };
+      userRequestAuthorizer.authorise.returns(Promise.reject(error));
+
+      filter.authCheckerUserOnlyFilter(req, res, error => {
+        expect(error.status).to.equal(502);
+        expect(error.error).to.equal('Bad Gateway');
+        expect(error.message).to.equal('blah connect ETIMEOUT blah');
+        done();
+      });
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "ECONNRESET" and no status', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah ECONNRESET blah'
+      };
+      userRequestAuthorizer.authorise.returns(Promise.reject(error));
+
+      filter.authCheckerUserOnlyFilter(req, res, error => {
+        expect(error.status).to.equal(502);
+        expect(error.error).to.equal('Bad Gateway');
+        expect(error.message).to.equal('blah ECONNRESET blah');
+        done();
+      });
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "ECONNREFUSED" and no status', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah ECONNREFUSED blah'
+      };
+      userRequestAuthorizer.authorise.returns(Promise.reject(error));
+
+      filter.authCheckerUserOnlyFilter(req, res, error => {
+        expect(error.status).to.equal(502);
+        expect(error.error).to.equal('Bad Gateway');
+        expect(error.message).to.equal('blah ECONNREFUSED blah');
+        done();
+      });
+    });
+
+    it('should return 500 status code in case of FetchError and error message is undefined', done => {
+      error = {
+        name: 'FetchError',
+        message: undefined
+      };
+      userRequestAuthorizer.authorise.returns(Promise.reject(error));
+
+      filter.authCheckerUserOnlyFilter(req, res, error => {
+        expect(error.status).to.equal(500);
+        expect(error.error).to.equal('Internal Server Error');
+        expect(error.message).to.equal(undefined);
+        done();
+      });
+    });
+
+
     it('should return 401 status code when idam call fails with no error status', done => {
       error = {};
       userRequestAuthorizer.authorise.returns(Promise.reject(error));
 
-      filter(req, res, error => {
+      filter.authCheckerUserOnlyFilter(req, res, error => {
         expect(error.status).to.equal(401);
         done();
       });
@@ -115,11 +245,137 @@ describe('authCheckerUserOnlyFilter', () => {
       };
       userRequestAuthorizer.authorise.returns(Promise.reject(error));
 
-      filter(req, res, error => {
+      filter.authCheckerUserOnlyFilter(req, res, error => {
         expect(error.status).to.equal(502);
         done();
       });
     });
-  });
 
+    it('should return 502 status code in case of FetchError and error message containing "getaddrinfo ENOTFOUND" next undefined', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah getaddrinfo ENOTFOUND blah'
+      };
+
+      // next undefined in function call
+      filter.mapFetchErrors(error , res);
+      
+      expect(res.status).to.equal(502);
+      expect(res.json.error).to.equal('Bad Gateway');
+      expect(res.json.message).to.equal('blah getaddrinfo ENOTFOUND blah');
+      done();
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "socket hang up" next undefined', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah socket hang up blah'
+      };
+
+      // next undefined in function call
+      filter.mapFetchErrors(error , res);
+      
+      expect(res.status).to.equal(502);
+      expect(res.json.error).to.equal('Bad Gateway');
+      expect(res.json.message).to.equal('blah socket hang up blah');
+      done();
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "getaddrinfo EAI_AGAIN" next undefined', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah getaddrinfo EAI_AGAIN blah'
+      };
+
+      // next undefined in function call
+      filter.mapFetchErrors(error , res);
+      
+      expect(res.status).to.equal(502);
+      expect(res.json.error).to.equal('Bad Gateway');
+      expect(res.json.message).to.equal('blah getaddrinfo EAI_AGAIN blah');
+      done();
+      });
+
+    it('should return 502 status code in case of FetchError and error message containing "connect ETIMEOUT" next undefined', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah connect ETIMEOUT blah'
+      };
+      
+      // next undefined in function call
+      filter.mapFetchErrors(error , res);
+      
+      expect(res.status).to.equal(502);
+      expect(res.json.error).to.equal('Bad Gateway');
+      expect(res.json.message).to.equal('blah connect ETIMEOUT blah');
+      done();
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "ECONNRESET" next undefined', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah ECONNRESET blah'
+      };
+
+      // next undefined in function call
+      filter.mapFetchErrors(error , res);
+      
+      expect(res.status).to.equal(502);
+      expect(res.json.error).to.equal('Bad Gateway');
+      expect(res.json.message).to.equal('blah ECONNRESET blah');
+      done();
+    });
+
+    it('should return 502 status code in case of FetchError and error message containing "ECONNREFUSED" next undefined', done => {
+      error = {
+        name: 'FetchError',
+        message: 'blah ECONNREFUSED blah'
+      };
+
+      // next undefined in function call
+      filter.mapFetchErrors(error , res);
+      
+      expect(res.status).to.equal(502);
+      expect(res.json.error).to.equal('Bad Gateway');
+      expect(res.json.message).to.equal('blah ECONNREFUSED blah');
+      done();
+
+    });
+
+
+    it('should return 500 status code in case of FetchError non matching error message and next undefined', done => {
+      error = {
+        name: 'FetchError',
+        message: 'some message',
+        status: 403
+      };
+
+      // next undefined in function call
+      filter.mapFetchErrors(error , res);
+      
+      expect(res.status).to.equal(500);
+      expect(res.json.error).to.equal('Error when connecting to remote server');
+      expect(res.json.status).to.equal(504);
+      expect(res.json.message).to.equal('some message');
+      done();
+    });
+
+    it('should return 500 status code in case of FetchError non matching error message undefined and next undefined', done => {
+      error = {
+        name: 'FetchError',
+        status: 403
+      };
+
+      // next undefined in function call
+      filter.mapFetchErrors(error , res);
+      
+      expect(res.status).to.equal(500);
+      expect(res.json.error).to.equal('Error when connecting to remote server');
+      expect(res.json.status).to.equal(504);
+      expect(res.json.message).to.equal(undefined);
+      done();
+    });
+
+
+  });
 });
