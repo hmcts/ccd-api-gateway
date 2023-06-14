b92b156568264b23ca7921a2c1f28cf172c56bf6
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-06-14 18:35:16 +0100

    Sample activity service to reduce costs (#487)
    * Sample activity service to reduce costs
    
    * Lint

--- app/app-insights/app-insights.js
+++ app/app-insights/app-insights.js
@@ -3,6 +3,16 @@ const appInsights = require('applicationinsights');
 
 const enabled = config.get('appInsights.enabled');
 
+function fineGrainedSampling(envelope) {
+  // activity data is not interesting and should not really be going through this proxy anyway
+  // when it was at 100% it was generating nearly 50% of HMCTS app insights data ingestion alone
+  if (envelope.data.baseType === 'RequestData' && envelope.data.baseData.name.includes('/activity')) {
+    envelope.sampleRate = 1;
+  }
+
+  return true;
+}
+
 const enableAppInsights = () => {
   if (enabled) {
     const appInsightsKey = config.get('secrets.ccd.AppInsightsInstrumentationKey');
@@ -11,6 +21,7 @@ const enableAppInsights = () => {
       .setAutoDependencyCorrelation(true)
       .setAutoCollectConsole(true, true);
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = appInsightsRoleName;
+    appInsights.defaultClient.addTelemetryProcessor(fineGrainedSampling);
     appInsights.start();
   }
 };
