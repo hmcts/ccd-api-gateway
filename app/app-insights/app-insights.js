f00ef5b925dd259127d59c04c4363c3d438c32a2
Tim Jacomb <timjacomb1@gmail.com>
2023-06-14 14:11:35 +0100

    Lint

--- app/app-insights/app-insights.js
+++ app/app-insights/app-insights.js
@@ -3,10 +3,10 @@ const appInsights = require('applicationinsights');
 
 const enabled = config.get('appInsights.enabled');
 
-function fineGrainedSampling(envelope, context) {
+function fineGrainedSampling(envelope) {
   // activity data is not interesting and should not really be going through this proxy anyway
   // when it was at 100% it was generating nearly 50% of HMCTS app insights data ingestion alone
-  if (envelope.data.baseType === 'RequestData' && envelope.data.baseData.name.includes("/activity")) {
+  if (envelope.data.baseType === 'RequestData' && envelope.data.baseData.name.includes('/activity')) {
     envelope.sampleRate = 1;
   }
 
