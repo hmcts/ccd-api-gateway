7526514190062619d34fa7857ee315f711fe6a7a
Anthony Dummer <anthony.dummer@HMCTS.net>
2026-05-01 09:12:35 +0100

    updating logs

--- app/util/log-safe-url.js
+++ app/util/log-safe-url.js
@@ -13,6 +13,35 @@ const SENSITIVE_QUERY_PARAMETERS = [
 
 const isAbsoluteUrl = value => /^[a-z][a-z\d+\-.]*:\/\//i.test(value);
 
+const redactQueryString = search => {
+  const searchParams = new URLSearchParams(search);
+
+  searchParams.forEach((queryValue, key) => {
+    if (SENSITIVE_QUERY_PARAMETERS.includes(key.toLowerCase())) {
+      searchParams.set(key, 'REDACTED');
+    }
+  });
+
+  const redactedSearch = searchParams.toString();
+  return redactedSearch ? `?${redactedSearch}` : '';
+};
+
+const redactRelativeUrl = value => {
+  const fragmentStart = value.indexOf('#');
+  const valueWithoutFragment = fragmentStart === -1 ? value : value.slice(0, fragmentStart);
+  const fragment = fragmentStart === -1 ? '' : value.slice(fragmentStart);
+  const queryStart = valueWithoutFragment.indexOf('?');
+
+  if (queryStart === -1) {
+    return value;
+  }
+
+  const path = valueWithoutFragment.slice(0, queryStart);
+  const query = valueWithoutFragment.slice(queryStart + 1);
+
+  return `${path}${redactQueryString(query)}${fragment}`;
+};
+
 const redactUrl = value => {
   if (!value) {
     return value;
@@ -20,16 +49,13 @@ const redactUrl = value => {
 
   try {
     const absoluteUrl = isAbsoluteUrl(value);
-    const parsed = new URL(value, absoluteUrl ? undefined : 'https://gateway.local');
+    if (!absoluteUrl) {
+      return redactRelativeUrl(value);
+    }
 
-    parsed.searchParams.forEach((queryValue, key) => {
-      if (SENSITIVE_QUERY_PARAMETERS.includes(key.toLowerCase())) {
-        parsed.searchParams.set(key, 'REDACTED');
-      }
-    });
-
-    const pathAndQuery = `${parsed.pathname}${parsed.search}`;
-    return absoluteUrl ? `${parsed.origin}${pathAndQuery}` : pathAndQuery;
+    const parsed = new URL(value);
+    const pathAndQuery = `${parsed.pathname}${redactQueryString(parsed.search)}`;
+    return `${parsed.origin}${pathAndQuery}`;
   } catch (error) {
     return value;
   }
