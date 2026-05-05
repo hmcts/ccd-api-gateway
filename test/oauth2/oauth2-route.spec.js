a21d9c0495589aa899a1ff0bed52e9c1c33ebf74
kaanaktas <aktaskaan@gmail.com>
2026-05-05 12:36:20 +0100

    Enforce stricter CORS origin validation and enhance cookie security with SameSite=Lax

--- test/oauth2/oauth2-route.spec.js
+++ test/oauth2/oauth2-route.spec.js
@@ -4,10 +4,14 @@ const proxyquire = require('proxyquire');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const sinonExpressMock = require('sinon-express-mock');
-const ACCESS_TOKEN_COOKIE_NAME = require('../../app/oauth2/oauth2-route').COOKIE_ACCESS_TOKEN;
+
+const ACCESS_TOKEN_COOKIE_NAME =
+  require('../../app/oauth2/oauth2-route').COOKIE_ACCESS_TOKEN;
+
 chai.use(sinonChai);
 
 describe('oauth2Route', () => {
+
   const TOKEN = {
     access_token: 'ey123.ey456',
     expires_in: 3600
@@ -22,96 +26,118 @@ describe('oauth2Route', () => {
   let responseFromPromiseMock;
 
   beforeEach(() => {
-
     config = {
       get: sinon.stub()
-    };
-    responseFromPromiseMock = {
-      status: 200,
-      json: sinon.stub()
     };
 
     request = sinonExpressMock.mockReq();
     response = sinonExpressMock.mockRes();
     next = sinon.stub();
+
+    responseFromPromiseMock = {
+      status: 200,
+      json: sinon.stub()
+    };
+
     accessTokenRequest = sinon.stub();
-    accessTokenRequest.withArgs(request).returns(Promise.resolve(responseFromPromiseMock));
+    accessTokenRequest.withArgs(request)
+      .returns(Promise.resolve(responseFromPromiseMock));
 
     oauth2Route = proxyquire('../../app/oauth2/oauth2-route', {
       './access-token-request': accessTokenRequest,
-      'config': config
+      config
     }).oauth2Route;
   });
 
-  it('should set an accessToken cookie with the "secure" flag enabled', done => {
+  it('should set accessToken cookie with secure flag enabled', (done) => {
 
     config.get.withArgs('security.secure_auth_cookie_enabled').returns(true);
-    responseFromPromiseMock.json.withArgs().returns(Promise.resolve(TOKEN));
+    responseFromPromiseMock.json.returns(Promise.resolve(TOKEN));
 
-    response.send.callsFake( () => {
-      try {
-
-        expect(accessTokenRequest).to.be.calledWith(request);
-        expect(config.get).to.be.calledWith('security.secure_auth_cookie_enabled');
-        expect(response.cookie).to.be.calledWith(ACCESS_TOKEN_COOKIE_NAME, TOKEN.access_token,
-          { maxAge: TOKEN.expires_in * 1000, httpOnly: true, secure: true });
-        expect(response.status).to.be.calledWith(204);
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
-
-    oauth2Route(request, response, next);
-  });
-
-  it('should set an accessToken cookie with the "secure" flag disabled', done => {
-
-    config.get.withArgs('security.secure_auth_cookie_enabled').returns(false);
-    responseFromPromiseMock.json.withArgs().returns(Promise.resolve(TOKEN));
-
+    response.status.callsFake(() => response);
     response.send.callsFake(() => {
       try {
-        expect(accessTokenRequest).to.be.calledWith(request);
-        expect(config.get).to.be.calledWith('security.secure_auth_cookie_enabled');
-        expect(response.cookie).to.be.calledWith(ACCESS_TOKEN_COOKIE_NAME, TOKEN.access_token,
-          { maxAge: TOKEN.expires_in * 1000, httpOnly: true, secure: false });
-        expect(response.status).to.be.calledWith(204);
+        expect(accessTokenRequest).to.have.been.calledWith(request);
+        expect(config.get).to.have.been.calledWith('security.secure_auth_cookie_enabled');
+
+        expect(response.cookie).to.have.been.calledWith(
+          ACCESS_TOKEN_COOKIE_NAME,
+          TOKEN.access_token,
+          {
+            maxAge: TOKEN.expires_in * 1000,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'Lax'
+          }
+        );
+
+        expect(response.status).to.have.been.calledWith(204);
+
         done();
-      } catch (e) {
-        done(e);
+      } catch (err) {
+        done(err);
       }
     });
 
     oauth2Route(request, response, next);
   });
 
-  it('should fail to obation an accessToken dude to unauthorized request.', done => {
+  it('should set accessToken cookie with secure flag disabled', (done) => {
 
-    let expectedError = {
+    config.get.withArgs('security.secure_auth_cookie_enabled').returns(false);
+    responseFromPromiseMock.json.returns(Promise.resolve(TOKEN));
+
+    response.status.callsFake(() => response);
+    response.send.callsFake(() => {
+      try {
+        expect(response.cookie).to.have.been.calledWith(
+          ACCESS_TOKEN_COOKIE_NAME,
+          TOKEN.access_token,
+          {
+            maxAge: TOKEN.expires_in * 1000,
+            httpOnly: true,
+            secure: false,
+            sameSite: 'Lax'
+          }
+        );
+
+        expect(response.status).to.have.been.calledWith(204);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+
+    oauth2Route(request, response, next);
+  });
+
+  it('should call next with error when token request fails', (done) => {
+
+    const expectedError = {
       status: 502,
       message: 'Internal Server Error'
     };
 
-    let  unauthorizedAccessTokenRequest = sinon.stub();
-    unauthorizedAccessTokenRequest.withArgs(request).returns(Promise.resolve(expectedError));
+    const failingRequest = sinon.stub()
+      .withArgs(request)
+      .returns(Promise.resolve(expectedError));
 
-    let unauthorizedOauth2Route = proxyquire('../../app/oauth2/oauth2-route', {
-      './access-token-request': unauthorizedAccessTokenRequest,
-      'config': config
+    const failingRoute = proxyquire('../../app/oauth2/oauth2-route', {
+      './access-token-request': failingRequest,
+      config
     }).oauth2Route;
 
-    next.callsFake((result) => {
+    next.callsFake((err) => {
       try {
-
-        expect(unauthorizedAccessTokenRequest).to.be.calledWith(request);
-        expect(result).to.eql(expectedError);
+        expect(failingRequest).to.have.been.calledWith(request);
+        expect(err).to.eql(expectedError);
         done();
       } catch (e) {
         done(e);
       }
     });
 
-    unauthorizedOauth2Route(request, response, next);
+    failingRoute(request, response, next);
   });
+
 });
